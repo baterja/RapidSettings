54c9f7b6bb7d15a778f5fe8c77df7fc8ccf33619
Michał Tomczak <tomczakmihal@gmail.com>
2018-04-07 15:16:10 +0200

    Added tests for FromAppSettingsProvider.

---
+++ RapidSettings.Tests/Providers/FromAppSettingsProviderTests.cs
@@ -0,0 +1,43 @@
+﻿#if !NETCOREAPP2_0 // NET Core AppSettings incorrectly gets config file in tests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidSettings.Exceptions;
+using RapidSettings.Providers;
+using System;
+using System.IO;
+
+namespace RapidSettings.Tests.Converters
+{
+    [TestClass]
+    public class FromAppSettingsProviderTests
+    {
+        [TestMethod]
+        public void FromAppSettingsProviderTest_SimpleRetrieval()
+        {
+            var fromAppSettingsProvider = new FromAppSettingsProvider();
+
+            var existingKeyValue = fromAppSettingsProvider.GetRawSetting("ExistingKey");
+
+            Assert.AreEqual("ExistingKeyValue", existingKeyValue);
+        }
+
+        [TestMethod]
+        public void FromAppSettingsProviderTest_NonExistingRetrieval()
+        {
+            var fromAppSettingsProvider = new FromAppSettingsProvider();
+
+            var notExistingKeyValue = fromAppSettingsProvider.GetRawSetting("NotExistingKey");
+
+            Assert.IsNull(notExistingKeyValue, "Not existing key value is not null!");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RapidSettingsException), AllowDerivedTypes = true)]
+        public void FromAppSettingsProviderTest_NullRetrieval()
+        {
+            var fromAppSettingsProvider = new FromAppSettingsProvider();
+
+            var nullKeyValue = fromAppSettingsProvider.GetRawSetting(null);
+        }
+    }
+}
+#endif
